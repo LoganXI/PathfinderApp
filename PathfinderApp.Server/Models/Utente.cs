b4a192ee7ffdb6ee1263478a1608f8a5737e19b2
Morgan Grande <morgan.grande@gmail.com>
2024-08-28 09:48:11 +0200

    Backend Base, inizio struttura backend per scafolding

---
+++ PathfinderApp.Server/Models/Utente.cs
@@ -0,0 +1,11 @@
+﻿namespace PathfinderApp.Server.Models
+{
+    public class Utente
+    {
+        public int UtenteId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public ICollection<Personaggio> Personaggi { get; set; }
+    }
+}
